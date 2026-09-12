9316af4c2f62b86ec298eaa7493bc673eac8680f
murilo-paula <murilo.pauladsz@gmail.com>
2026-09-12 10:49:49 -0300

    feat: Use CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
-  imports: [
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'estudo-nestjs-prisma',
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
 })
 export class AppModule {}
