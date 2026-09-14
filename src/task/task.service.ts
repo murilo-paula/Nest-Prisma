3d571c9d4bc8e92573e0828c47818fc05308d278
murilo-paula <murilo.pauladsz@gmail.com>
2026-09-14 11:01:29 -0300

    feat: test-bcrypt

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 export interface Task {
     name: string;
     id: number;
+    password: string;
     title: string;
     done: boolean;
     active: boolean;
@@ -12,10 +13,10 @@ export interface Task {
 @Injectable()
 export class TaskService {
     private tasks: Task[] = [
-        {id: 1, name: 'murilo', title: 'estudar nestjs', done: false, active: true},
-        {id: 2, name: 'maria', title: 'estudar prisma', done: false, active: false},
-        {id: 3, name: 'joão', title: 'estudar react', done: false, active: true},
-        {id: 4, name: 'mateus', title: 'estudar tailwind', done: true, active: true},
+        {id: 1, name: 'murilo', password: '3282382849', title: 'estudar nestjs', done: false, active: true},
+        {id: 2, name: 'maria', password: '34534537474', title: 'estudar prisma', done: false, active: true},
+        {id: 3, name: 'joão', password: '63623625245', title: 'estudar react', done: false, active: true},
+        {id: 4, name: 'mateus', password: '1234', title: 'estudar tailwind', done: true, active: true},
     ];
 
     findOne(id: string) {
@@ -23,15 +24,17 @@ export class TaskService {
     }
 
     findAll() {
-        const newTasks = this.tasks.map(i => i.active === true ? null : i);
+        const newTasks = this.tasks.map(i => i.active === false ? null : i);
         return newTasks;
     }
 
     create(body: any): Task {
+
         const newTask: Task = {
             name: body.name,
             id: this.tasks.length + 1,
             title: body.title,
+            password: body.password,
             done: false,
             active: true
         }
